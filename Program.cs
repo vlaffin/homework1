74ed2677622d4c487cc04f24affc8f58e6e9a569
vlad <shutikov-vlad@mail.ru>
2023-01-18 21:16:09 +0300

    1 задача

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//1 задача
+
+Console.WriteLine("введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1<number2)
+{
+Console.WriteLine ("наибольшее число = " +  number2);
+Console.WriteLine ("наименьшее число = " +  number1);
+}
+else
+{
+Console.WriteLine ("наибольшее число = " + number1);
+Console.WriteLine ("наименьшее число = " + number2);
+}
+
