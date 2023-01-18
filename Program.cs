e256566cffce44edae9ce500ada9603e852d85ea
vlad <shutikov-vlad@mail.ru>
2023-01-18 23:01:21 +0300

    написали 2 и 3 задачу

--- Program.cs
+++ Program.cs
@@ -1,20 +1,50 @@
 ﻿//1 задача
 
-Console.WriteLine("введите первое число");
-int number1 = int.Parse(Console.ReadLine());
+//Console.Write("введите первое число: ");
+//int number1 = int.Parse(Console.ReadLine());
 
 
-Console.WriteLine("введите второе число");
-int number2 = int.Parse(Console.ReadLine());
+//Console.Write("введите второе число: ");
+//int number2 = int.Parse(Console.ReadLine());
 
-if (number1<number2)
-{
-Console.WriteLine ("наибольшее число = " +  number2);
-Console.WriteLine ("наименьшее число = " +  number1);
-}
-else
-{
-Console.WriteLine ("наибольшее число = " + number1);
-Console.WriteLine ("наименьшее число = " + number2);
-}
 
+//if (number1<number2)  
+//{
+//Console.WriteLine("наибольшее число = " +  number2);
+//Console.WriteLine("наименьшее число = " +  number1);
+//}
+//else
+//{
+//Console.WriteLine("наибольшее число = " + number1);
+//Console.WriteLine("наименьшее число = " + number2);
+//}
+
+//2 задача
+
+ //Console.Write("Введите первое число: ");
+//int max = int.Parse(Console.ReadLine());
+ //Console.Write("Введите второе число: ");
+//int numberB = int.Parse(Console.ReadLine());
+//Console.Write("Введите третье число: ");
+//int numberC = int.Parse(Console.ReadLine());
+ //if (max < numberB)
+//{
+// max = numberB;
+//}
+ //if (max < numberC)
+//{
+// max = numberC;
+//}
+// Console.WriteLine("Максимальное число из трёх : " + max);
+
+//3 задача
+
+//Console.Write("введите число,а мы скажем является ли оно чётным :");
+//int number1 = int.Parse(Console.ReadLine());
+
+//if (number1 % 2 == 0)
+//{
+//    Console.Write("число: " + number1 + " является чётным");
+//}
+//else
+//Console.Write(number1 +" - это число не является чётным, попробуйте другое число");
